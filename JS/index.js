2d4e0b8b04fe0b366d22863a80fe7502d3087eb0
Ahmedaydiarra <104395637+Ahmedaydiarra@users.noreply.github.com>
2023-02-16 11:53:26 +0100

    js

---
+++ JS/index.js
@@ -0,0 +1,61 @@
+// Get the modal
+var modal = document.getElementById("signupModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("signupLink");
+
+// Get the <span> element that closes the modal
+var cross = document.getElementById("closeSignupModal");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+cross.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Caroussel
+
+        // set the default active slide to the first one
+        let slideIndex = 1;
+        showSlide(slideIndex);
+
+        // change slide with the prev/next button
+        function moveSlide(moveStep) {
+            showSlide(slideIndex += moveStep);
+        }
+
+        // change slide with the dots
+        function currentSlide(n) {
+            showSlide(slideIndex = n);
+        }
+
+        function showSlide(n) {
+            let i;
+            const slides = document.getElementsByClassName("slide");
+            const dots = document.getElementsByClassName('dot');
+            
+            if (n > slides.length) { slideIndex = 1 }
+            if (n < 1) { slideIndex = slides.length }
+
+            // hide all slides
+            for (i = 0; i < slides.length; i++) {
+                slides[i].classList.add('hidden');
+            }
+
+            // remove active status from all dots
+            for (i = 0; i < dots.length; i++) {
+                dots[i].classList.remove('bg-orange');
+                dots[i].classList.add('bg-blue');
+            }
+
+            // show the active slide
+            slides[slideIndex - 1].classList.remove('hidden');
+
+            // highlight the active dot
+            dots[slideIndex - 1].classList.remove('bg-blue');
+            dots[slideIndex - 1].classList.add('bg-orange');
+        }
